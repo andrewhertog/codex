259a608695896c0dc957d8b3b5642338fca7f9e7
Andrew den Hertog <andrew@denhertog.ca>
2024-07-11 21:21:00 -0400

    change where i load extensions
    Signed-off-by: Andrew den Hertog <andrew@denhertog.ca>

--- get-extensions.sh
+++ get-extensions.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
+echo $(pwd)
 
-jsonfile=$(cat extensions.json)
-extensions_dir=./vscode/extensions
+jsonfile=../extensions.json
+extensions_dir=./extensions
 base_dir=$(pwd)
 
-count=$(jq -r '. | length' extensions.json)
+count=$(jq -r '. | length' ${jsonfile})
 for i in $(seq $count); do
-  url=$(jq -r ".[$i-1].url" extensions.json)
-  name=$(jq -r ".[$i-1].name" extensions.json)
+  url=$(jq -r ".[$i-1].url" ${jsonfile})
+  name=$(jq -r ".[$i-1].name" ${jsonfile})
   echo $name $url
   mkdir -p ${extensions_dir}/"$name"
   curl -L -o "$name".zip "$url"
   unzip "$name".zip -d ${extensions_dir}/"$name"
   mv ${extensions_dir}/"$name"/extension/* ${extensions_dir}/"$name"/
   cd ${extensions_dir}/"$name"
-  if [ -d "node_modules" ]; then
-    rm -rf node_modules
-    npm install --omit=dev
-  fi
-  if [ -d "out" ]; then
-    rm -rf out
-  fi
   cd ${base_dir}
   rm "$name".zip
 done
